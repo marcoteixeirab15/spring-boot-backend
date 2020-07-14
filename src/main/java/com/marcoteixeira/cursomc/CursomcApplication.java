74c425dd01590cc78ea76e803cca517d605c5392
Marco Barbosa <marco.teixeirab15@gmail.com>
2020-07-14 00:39:16 -0300

    Criando operação de instanciação

--- src/main/java/com/marcoteixeira/cursomc/CursomcApplication.java
+++ src/main/java/com/marcoteixeira/cursomc/CursomcApplication.java
@@ -1,15 +1,34 @@
 package com.marcoteixeira.cursomc;
 
+import com.marcoteixeira.cursomc.domain.Categoria;
+import com.marcoteixeira.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
     }
 
+    private CategoriaRepository categoriaRepository;
+
+    public CursomcApplication(CategoriaRepository categoriaRepository) {
+        this.categoriaRepository = categoriaRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria categoria1 = new Categoria(null, "Informática");
+        Categoria categoria2 = new Categoria(null, "Escritório");
+
+        categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+    }
 }

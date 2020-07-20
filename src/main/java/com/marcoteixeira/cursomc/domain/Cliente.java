98635cc0e816ec47b53387de61f7db3cbcf66479
Marco Barbosa <marco.teixeirab15@gmail.com>
2020-07-20 17:16:09 -0300

    Ajustando a delecao de cliente

--- src/main/java/com/marcoteixeira/cursomc/domain/Cliente.java
+++ src/main/java/com/marcoteixeira/cursomc/domain/Cliente.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.marcoteixeira.cursomc.domain.enums.TipoCliente;
 
+import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
@@ -36,7 +37,7 @@ public class Cliente implements Serializable {
 
     private Integer tipo;
 
-    @OneToMany(mappedBy = "cliente")
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
     private List<Endereco> enderecos = new ArrayList<>();
 
     @ElementCollection

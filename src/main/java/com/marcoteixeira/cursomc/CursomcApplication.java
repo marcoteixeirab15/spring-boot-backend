019300ae6702a13b7e8d8311003aba21c583eed9
Marco Barbosa <marco.teixeirab15@gmail.com>
2020-07-22 00:19:14 -0300

    Criando profile de Teste

--- src/main/java/com/marcoteixeira/cursomc/CursomcApplication.java
+++ src/main/java/com/marcoteixeira/cursomc/CursomcApplication.java
@@ -33,36 +33,7 @@ import java.util.Collections;
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
-    private final CategoriaRepository categoriaRepository;
-    private final ProdutoRepository produtoRepository;
-    private final EstadoRepository estadoRepository;
-    private final CidadeRepository cidadeRepository;
-    private final ClienteRepository clienteRepository;
-    private final EnderecoRepository enderecoRepository;
-    private final PedidoRepository pedidoRepository;
-    private final PagamentoRepository pagamentoRepository;
-    private final ItemPedidoRepository itemPedidoRepository;
 
-
-    public CursomcApplication(CategoriaRepository categoriaRepository,
-                              ProdutoRepository produtoRepository,
-                              EstadoRepository estadoRepository,
-                              CidadeRepository cidadeRepository,
-                              ClienteRepository clienteRepository,
-                              EnderecoRepository enderecoRepository,
-                              PedidoRepository pedidoRepository,
-                              PagamentoRepository pagamentoRepository,
-                              ItemPedidoRepository itemPedidoRepository) {
-        this.categoriaRepository = categoriaRepository;
-        this.produtoRepository = produtoRepository;
-        this.estadoRepository = estadoRepository;
-        this.cidadeRepository = cidadeRepository;
-        this.clienteRepository = clienteRepository;
-        this.enderecoRepository = enderecoRepository;
-        this.pedidoRepository = pedidoRepository;
-        this.pagamentoRepository = pagamentoRepository;
-        this.itemPedidoRepository = itemPedidoRepository;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
@@ -70,102 +41,7 @@ public class CursomcApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Categoria categoria1 = new Categoria(null, "Informática");
-        Categoria categoria2 = new Categoria(null, "Escritório");
-        Categoria categoria3 = new Categoria(null, "Cama mesa e banho");
-        Categoria categoria4 = new Categoria(null, "Eletronicos");
-        Categoria categoria5 = new Categoria(null, "Jardinagem");
-        Categoria categoria6 = new Categoria(null, "Decoração");
-        Categoria categoria7 = new Categoria(null, "Perfumaria");
 
-        Produto produto1 = new Produto(null, "Computador", 2000.00);
-        Produto produto2 = new Produto(null, "Impressora", 800.00);
-        Produto produto3 = new Produto(null, "Mouse", 80.00);
-        Produto produto4 = new Produto(null, "Mesa de escritório", 300.00);
-        Produto produto5 = new Produto(null, "Toalha", 50.00);
-        Produto produto6 = new Produto(null, "Colcha", 200.00);
-        Produto produto7 = new Produto(null, "TV true color", 1200.00);
-        Produto produto8 = new Produto(null, "Roçadeira", 800.00);
-        Produto produto9 = new Produto(null, "Abajour", 100.00);
-        Produto produto10 = new Produto(null, "Pendente", 100.00);
-        Produto produto11 = new Produto(null, "Shampoo", 90.00);
-
-        categoria1.getProdutos().addAll(Arrays.asList(produto1, produto2, produto3));
-        categoria2.getProdutos().addAll(Arrays.asList(produto2, produto4));
-        categoria3.getProdutos().addAll(Arrays.asList(produto5, produto6));
-        categoria4.getProdutos().addAll(Arrays.asList(produto1, produto2, produto3, produto7));
-        categoria5.getProdutos().addAll(Collections.singletonList(produto6));
-        categoria6.getProdutos().addAll(Arrays.asList(produto9, produto10));
-        categoria7.getProdutos().addAll(Collections.singletonList(produto11));
-
-        produto1.getCategorias().addAll(Collections.singletonList(categoria1));
-        produto2.getCategorias().addAll(Arrays.asList(categoria1, categoria2));
-        produto3.getCategorias().addAll(Collections.singletonList(categoria1));
-        produto4.getCategorias().addAll(Collections.singletonList(categoria2));
-        produto5.getCategorias().addAll(Collections.singletonList(categoria3));
-        produto6.getCategorias().addAll(Collections.singletonList(categoria3));
-        produto7.getCategorias().addAll(Collections.singletonList(categoria4));
-        produto8.getCategorias().addAll(Collections.singletonList(categoria5));
-        produto9.getCategorias().addAll(Collections.singletonList(categoria6));
-        produto10.getCategorias().addAll(Collections.singletonList(categoria6));
-        produto11.getCategorias().addAll(Collections.singletonList(categoria7));
-
-        categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2, categoria3, categoria4, categoria5, categoria6, categoria7));
-        produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3, produto4, produto5, produto6, produto7, produto8, produto9, produto10, produto11));
-
-        Estado estado1 = new Estado(null, "Minas Gerais");
-        Estado estado2 = new Estado(null, "São Paulo");
-
-        Cidade cidade1 = new Cidade(null, "Uberlandia", estado1);
-        Cidade cidade2 = new Cidade(null, "São Paulo", estado2);
-        Cidade cidade3 = new Cidade(null, "Campinas", estado2);
-
-        estado1.getCidades().addAll(Collections.singletonList(cidade1));
-        estado2.getCidades().addAll(Arrays.asList(cidade2, cidade3));
-
-        estadoRepository.saveAll(Arrays.asList(estado1, estado2));
-        cidadeRepository.saveAll(Arrays.asList(cidade1, cidade2, cidade3));
-
-        Cliente cliente1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOA_FISICA);
-
-        cliente1.getTelefone().addAll(Arrays.asList("33937720", "982523034"));
-
-        Endereco endereco1 = new Endereco(null, "Rua flores", "300", "Apto 303", "Jardim", "72503509", cliente1, cidade1);
-        Endereco endereco2 = new Endereco(null, "Avenida Martins", "105", "Sala 800", "Centro", "72503512", cliente1, cidade2);
-
-        cliente1.getEnderecos().addAll(Arrays.asList(endereco1, endereco2));
-
-        clienteRepository.saveAll(Collections.singletonList(cliente1));
-        enderecoRepository.saveAll(Arrays.asList(endereco1, endereco2));
-
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
-
-        Pedido pedido1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cliente1, endereco1);
-        Pedido pedido2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cliente1, endereco2);
-
-        Pagamento pagamento1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, pedido1, 6);
-        pedido1.setPagamento(pagamento1);
-
-        Pagamento pagamento2 = new PagamentoBoleto(null, EstadoPagamento.PENDENTE, pedido2, sdf.parse("20/10/2017 00:00"), null);
-        pedido2.setPagamento(pagamento2);
-
-        cliente1.getPedidos().addAll(Arrays.asList(pedido1, pedido2));
-
-        pedidoRepository.saveAll(Arrays.asList(pedido1, pedido2));
-        pagamentoRepository.saveAll(Arrays.asList(pagamento1, pagamento2));
-
-        ItemPedido itemPedido1 = new ItemPedido(pedido1, produto1, 0.00, 1, 2000.000);
-        ItemPedido itemPedido2 = new ItemPedido(pedido1, produto3, 0.00, 2, 80.000);
-        ItemPedido itemPedido3 = new ItemPedido(pedido2, produto2, 100.00, 1, 800.000);
-
-        pedido1.getItens().addAll(Arrays.asList(itemPedido1, itemPedido2));
-        pedido2.getItens().addAll(Collections.singletonList(itemPedido3));
-
-        produto1.getItens().addAll(Collections.singletonList(itemPedido1));
-        produto2.getItens().addAll(Collections.singletonList(itemPedido3));
-        produto3.getItens().addAll(Collections.singletonList(itemPedido2));
-
-        itemPedidoRepository.saveAll(Arrays.asList(itemPedido1, itemPedido2, itemPedido3));
 
 
     }
---
+++ src/main/java/com/marcoteixeira/cursomc/config/TestConfig.java
@@ -0,0 +1,28 @@
+package com.marcoteixeira.cursomc.config;
+
+import com.marcoteixeira.cursomc.services.DbService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+    private final DbService dbService;
+
+    public TestConfig(DbService dbService) {
+        this.dbService = dbService;
+    }
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+        dbService.instantiateDatabase();
+
+
+        return true;
+    }
+
+}
